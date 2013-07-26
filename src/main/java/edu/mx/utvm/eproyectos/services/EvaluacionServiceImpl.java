121c793d149389d1c3ad619d549bcad344986e6e
Mario Rivera <mrivera@pc-sistemas-mrivera>
2013-07-26 08:57:02 -0500

    BDD, TDD sobre servicios, mockeando objetos

--- src/main/java/edu/mx/utvm/eproyectos/services/EvaluacionServiceImpl.java
+++ src/main/java/edu/mx/utvm/eproyectos/services/EvaluacionServiceImpl.java
@@ -1,19 +1,26 @@
 package edu.mx.utvm.eproyectos.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.mx.utvm.eproyectos.dao.EvaluacionDao;
 import edu.mx.utvm.eproyectos.model.Evaluacion;
 
 public class EvaluacionServiceImpl implements EvaluacionService{
 
+
+	@Autowired
+	private EvaluacionDao dao;
+	
 	@Override
-	public void save(Evaluacion evaluacion) {
-		// TODO Auto-generated method stub
+	public void save(Evaluacion evaluacion) {		
 		
 	}
 
 	@Override
 	public Evaluacion read(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Evaluacion read = dao.read(id);
+		dao.create(new Evaluacion(1, ""));
+		return read;
 	}
 
 }
--- src/test/java/edu/mx/utvm/eproyectos/services/TestEvaluacionService.java
+++ src/test/java/edu/mx/utvm/eproyectos/services/TestEvaluacionService.java
@@ -1,41 +1,48 @@
 package edu.mx.utvm.eproyectos.services;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import junit.framework.Assert;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import edu.mx.utvm.eproyectos.dao.EvaluacionDao;
 import edu.mx.utvm.eproyectos.model.Evaluacion;
 @RunWith(MockitoJUnitRunner.class)
 public class TestEvaluacionService {	
 	
+	@Mock
+	private EvaluacionDao dao;
+	
+	@InjectMocks
+	private EvaluacionService service = new EvaluacionServiceImpl();
+	
 	@Test
 	public void testComportamientoSaveDelServicio(){
-		// Creacion de los mocks
-		EvaluacionService service = mock(EvaluacionServiceImpl.class); 
-		Evaluacion evaluacion = mock(Evaluacion.class);
-		
-		service.save(evaluacion);
-		
-		// definiendo comportamiento
-		when(service.read(213460)).thenReturn(evaluacion);
-		when(service.read(-1)).thenReturn(null);
-		when(service.read(0)).thenReturn(null);
-		
-		Evaluacion read = service.read(213460);
-		Evaluacion readNull = service.read(-1);
-		Evaluacion readNullParamCero = service.read(0);
 
-		// se verifica que el metodo save solo se utilize una vez
-		verify(service, times(1)).save(evaluacion);
-
-		// se verifica que el metodo save solo se utilize una vez
-		verify(service, times(1)).read(213460);
-		verify(service, times(1)).read(-1);
-		verify(service, times(1)).read(0);
+		Evaluacion evaluacion = Mockito.mock(Evaluacion.class);
+		
+		// resultado esperado
+		when(dao.read(1)).thenReturn(evaluacion);
+		when(dao.read(0)).thenReturn(null);
+		
+		// invocacion
+		/*Evaluacion read = service.read(-1);		
+		Assert.assertNull(read);*/
+		
+		Evaluacion read = service.read(0);
+		Assert.assertNull(read);
+		read = service.read(1);
+		Assert.assertNotNull(read);
+		
+		// comprobacion
+		verify(dao, times(1)).read(1);
+		verify(dao, times(1)).read(0);
 	}
 }
