6753448019463a60a9c1d0b6dc96337e7ce6afcb
v2r3 <paznogard@hotmail.com>
2013-07-26 13:33:59 -0500

    Cambios en sprin mvc

--- src/main/java/edu/mx/utvm/eproyectos/controllers/IndexController.java
+++ src/main/java/edu/mx/utvm/eproyectos/controllers/IndexController.java
@@ -11,31 +11,14 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.SessionAttributes;
-=======
->>>>>>> c54b648dd1c205e3d59bf4cf5bcec723b58989a6
+
+
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
-<<<<<<< HEAD
-	
-	// Este es loguer
-	protected final Log log = LogFactory.getLog(getClass());	
 
-
-    @RequestMapping(value="/login",  method = RequestMethod.GET)
-    public ModelAndView handleIndexRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-		ModelAndView model = new ModelAndView("login");
-		return model;
-    }
-	
-   
-	
-=======
 
 	protected final Log log = LogFactory.getLog(getClass());
 
@@ -46,5 +29,4 @@ public class IndexController {
 		return model;
 	}
 
->>>>>>> c54b648dd1c205e3d59bf4cf5bcec723b58989a6
 }
